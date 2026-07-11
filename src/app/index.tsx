37b130933c8f46415876fc6f5318a358beb347be
modern-agency <jeanhonoriodemelo@gmail.com>
2026-07-11 14:11:59 -0300

    first commit

---
+++ src/app/index.tsx
@@ -0,0 +1,111 @@
+import { Link } from "expo-router";
+import { useState } from "react";
+
+import {
+  Image,
+  KeyboardAvoidingView,
+  Platform,
+  ScrollView,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
+
+import { Button } from "@/components/Button";
+import { Input } from "@/components/input";
+
+export default function Index() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  function handleSignIn() {
+    //Alert.alert("Entrar", "Função de entrar acionada!");
+    console.log(email, password);
+  }
+
+  return (
+    <KeyboardAvoidingView
+      style={{ flex: 1 }}
+      behavior={Platform.select({
+        ios: "padding",
+        android: "height",
+      })}
+    >
+      <ScrollView
+        contentContainerStyle={{ flexGrow: 1 }}
+        keyboardShouldPersistTaps="handled"
+        showsVerticalScrollIndicator={false}
+      >
+        <View style={styles.container}>
+          <Image
+            source={require("@/assets/img1.png")}
+            style={styles.illustration}
+          />
+
+          <Text style={styles.title}>Entrar</Text>
+          <Text style={styles.subtitle}>
+            Acesse sua conta com e-mail e senha
+          </Text>
+
+          <View style={styles.form}>
+            <Input
+              placeholder="E-mail"
+              keyboardType="email-address"
+              value={email}
+              onChangeText={setEmail}
+            />
+
+            <Input
+              placeholder="Senha"
+              secureTextEntry
+              value={password}
+              onChangeText={setPassword}
+            />
+
+            <Button label="Entrar" onPress={handleSignIn} />
+          </View>
+
+          <Text style={styles.fonteText}>
+            Não tem uma conta?{" "}
+            <Link
+              href="/signup"
+              style={{ color: "#000000", fontWeight: "bold" }}
+            >
+              Cadastre-se aqui.
+            </Link>
+          </Text>
+        </View>
+      </ScrollView>
+    </KeyboardAvoidingView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#FDFDFD",
+    padding: 32,
+  },
+  illustration: {
+    width: "100%",
+    height: 330,
+    resizeMode: "contain",
+    marginTop: 62,
+  },
+  title: {
+    fontSize: 32,
+    fontWeight: "900",
+  },
+  subtitle: {
+    fontSize: 16,
+  },
+  form: {
+    marginTop: 24,
+    gap: 12,
+  },
+  fonteText: {
+    textAlign: "center",
+    marginTop: 24,
+    color: "#585860",
+  },
+});
